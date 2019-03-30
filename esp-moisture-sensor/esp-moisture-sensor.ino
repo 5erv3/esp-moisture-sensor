df676ed2d54313351fb31adb44ebd06c8fd1484e
stefan <spicker@outlook.com>
2019-03-30 14:21:56 +0100

    initial commit

---
+++ esp-moisture-sensor/esp-moisture-sensor.ino
@@ -0,0 +1,106 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include "config.h"
+
+// uncomment logging when not needed
+#define LOGGING  1
+
+#define TIME_SLEEPTIME_S          3600
+#define TIME_FACTOR_US_TO_S       (1000*1000)
+#define WIFI_MAX_CONNECT_TIME_SEC 30
+#define MQTT_RECONNECT_ATTEMPTS   3
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+char buf[100];
+int reconnect_counter = 0;
+
+void LOG(const char* logstring, bool newline = true){
+  #if LOGGING
+  if (newline){
+    Serial.println(logstring);
+  } else {      
+    Serial.print(logstring);
+  }
+  #endif
+}
+
+void setup() {
+  pinMode(moistureSensPin, INPUT);
+#if LOGGING
+  Serial.begin(115200);
+#endif
+  
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+}
+
+int deepsleep(long sleeptime_us = TIME_FACTOR_US_TO_S * TIME_SLEEPTIME_S){
+  sprintf(buf, "Going to deepsleep for %d seconds...", sleeptime_us/TIME_FACTOR_US_TO_S);
+  LOG(buf);
+
+  esp_deep_sleep(sleeptime_us);
+  esp_deep_sleep_start();
+}
+
+void setup_wifi() {
+  delay(10);
+
+  sprintf(buf, "Connecting to %s...", wifi_ssid);
+  LOG(buf);
+
+  WiFi.begin(wifi_ssid, wifi_password);
+
+  reconnect_counter = 0;
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    reconnect_counter ++;
+    if (reconnect_counter >= WIFI_MAX_CONNECT_TIME_SEC){
+      LOG("ERROR: WiFi connection counter expired");
+      deepsleep();
+    }
+  }  
+  sprintf(buf, "WiFi connected, IP address: ");
+  LOG(buf, false);
+  Serial.print(WiFi.localIP());
+  LOG("");
+}
+
+void reconnect() {
+  reconnect_counter = 0;
+  while (!client.connected()) {
+    LOG("Attempting MQTT connection...");
+    if (client.connect("ESP8266Client")) {
+      LOG("connected", true);
+    } else {
+      reconnect_counter++;
+      if (reconnect_counter >= MQTT_RECONNECT_ATTEMPTS){
+        LOG("ERROR: too many mqtt attempts");
+        deepsleep();
+      }      
+      sprintf(buf, "connection failed, rc %d", client.state());
+      LOG(buf);      
+      LOG(" try again in 5 seconds", true);
+      delay(5000);
+    }
+  }
+}
+
+int getMoisture(){
+  return analogRead(moistureSensPin);
+}
+
+void loop() {
+  
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  sprintf(buf, "%d", getMoisture());
+
+  client.publish(mqtt_topic, buf);
+  deepsleep();
+}
